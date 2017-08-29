9fc5c37a8df50445533b3747ec3c5a3caef07ca4
ZeSly <zesly42@gmail.com>
2017-08-29 19:38:39 +0200

    Get power voltage command
    Added a bad command test + test corrections

--- lx200_protocol.c
+++ lx200_protocol.c
@@ -60,6 +60,34 @@ void GetTelescopeProductName()
     strcpy(LX200Response, "ZeGoto#");
 }
 
+void GetPowerVoltage()
+{
+    // POWER_SENSE is on AN10 
+    double ADCValue;
+    
+    AD1PCFGL = 0xFBFF;  // AN10 as analog, all other pins are digital
+    AD1CON1 = 0x0000;   // SAMP bit = 0 ends sampling and starts converting
+    AD1CHS = 0x000A;    // Connect AN10 as S/H+ input
+    
+    AD1CSSL = 0;
+    AD1CON3 = 0x0002; // Manual Sample, Tad = 3Tcy
+    AD1CON2 = 0;
+    AD1CON1bits.ADON = 1; // turn ADC ON
+
+    AD1CON1bits.SAMP = 1; // start sampling...
+    Delay10us(1); // Ensure the correct sampling time has elapsed
+    // before starting conversion.
+    AD1CON1bits.SAMP = 0; // start converting
+    while (!AD1CON1bits.DONE)
+    {
+    }; // conversion done?
+    ADCValue = ADC1BUF0; // yes then get ADC value
+    ADCValue *= 3.3 * 9.5 + 0.5;   // Vref * voltage_divider + D1 voltage drop
+    ADCValue /= 1023.0;
+    
+    sprintf(LX200Response, "%f#", ADCValue);
+}
+
 static APP_CONFIG newAppConfig;
 static BOOL StartnewAppConfig = TRUE;
 
@@ -227,6 +255,8 @@ LX200Command LX200CommandTab[] ={
     { "GVN", 3, GetTelescopeFirmwareNumber},
     { "GVP", 3, GetTelescopeProductName},
     { "GVT", 3, GetTelescopeFirmwareTime},
+    
+    { "GPV", 3, GetPowerVoltage},
 
     { "Q", 1, Halt},
     { "Me", 2, MoveEast},
